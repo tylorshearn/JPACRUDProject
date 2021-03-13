7f5b0d33c939ac8097ebb0e03eebd17962f0fb8a
tylorshearn <tylorshearn95@gmail.com>
2021-03-12 19:30:49 -0500

    Stub Boot/MVC Project, Link JPA Dependency

---
+++ StockCRUD/src/main/java/com/skilldistillery/stock/StockCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockCrudApplication.class, args);
+	}
+
+}
